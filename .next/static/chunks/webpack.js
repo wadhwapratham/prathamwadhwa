bdc8a322817694887987c09d4c44b167d220ee22
wadhwapratham <117077132+wadhwapratham@users.noreply.github.com>
2024-10-24 13:40:44 +0530

    project update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6432180bebedc2d5"; }
+/******/ 		__webpack_require__.h = function() { return "dfb0506fb04fce3d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/project.js
+++ pages/project.js
@@ -17,47 +17,51 @@ export default function Project(){
                 <section className="list-projects">
                     <div className="container">
                         <ul className="list-lates">
-                            <li><a target="_blank" href="https://www.kawaiiamai.com">Kawaii Amai - https://www.kawaiiamai.com</a></li>
-                            <li><a target="_blank" href="https://mayandco.in">Mayandco - https://mayandco.in</a></li>
-                            <li><a target="_blank" href="https://www.newu.in">Newu - https://www.newu.in</a></li>
-                            <li><a target="_blank" href="https://jaqulineusa.com">Jaquline USA - https://jaqulineusa.com</a></li>
-                            <li><a target="_blank" href="https://amala.earth">Amala Earth - https://amala.earth</a></li>
-                            <li><a target="_blank" href="https://www.govo.life">Govo life - https://www.govo.life</a></li>
-                            <li><a target="_blank" href="https://www.biotique.com">Biotique - https://www.biotique.com</a></li>
-                            <li><a target="_blank" href="https://activbeaute.com">Active Beaute - https://activbeaute.com</a></li>
-                            <li><a target="_blank" href="https://www.yahvi.co.in">Yahvi - https://www.yahvi.co.in</a></li>
-                            <li><a target="_blank" href="https://chicasuu.com">Chicasuu - https://chicasuu.com</a></li>
-                            <li><a target="_blank" href="https://millesupergrain.com">MilleSuperGrain - https://millesupergrain.com</a></li>
-                            <li><a target="_blank" href="https://www.instax.in">Instax FujiFilms - https://www.instax.in</a></li>
-                            <li><a target="_blank" href="https://www.klfnirmal.com">KLF Nirmal : https://www.klfnirmal.com</a></li>
-                            <li><a target="_blank" href="https://membersonly.com">Members Only : https://membersonly.com</a></li>
-                            <li><a target="_blank" href="https://neutraleafindia.com">Neutraleaf India : https://neutraleafindia.com</a></li>
-                            <li><a target="_blank" href="https://ary-fj.myshopify.com">Araya : https://ary-fj.myshopify.com</a></li>
-                            <li><a target="_blank" href="#">Devatri Wellness</a></li>
-                            <li><a target="_blank" href="https://www.haldiramuk.com/">HaldiramsUk : https://www.haldiramuk.com/</a></li>
-                            <li><a target="_blank" href="https://main-hardwares.myshopify.com">Main Hardware : https://main-hardwares.myshopify.com</a></li>
-                            <li><a target="_blank" href="https://grabtocart-store.myshopify.com">Grab To Cart : https://grabtocart-store.myshopify.com</a></li>
-                            <li><a target="_blank" href="https://www.reranger.com">ReRanger : https://www.reranger.com</a></li>
-                            <li><a target="_blank" href="https://www.petland.ca/">Petland Canada : https:// petland.ca</a></li>
-                            <li><a target="_blank" href="https://randolphusa.com">Randolph USA : https://randolphusa.com</a></li>
-                            <li><a target="_blank" href="https://yufoodlabs.com">yufoodlabs : https://yufoodlabs.com</a></li>
-                            <li><a target="_blank" href="https://xtepindia.com/">Xtep : https://xtepindia.com/</a></li>
-                            <li><a target="_blank" href="https://ebec94-3.myshopify.com/">Boito https://ebec94-3.myshopify.com</a></li>
-                            <li><a target="_blank" href="https://confimen.in/">Confimen https://confimen.in</a></li>
-                            <li><a target="_blank" href="https://pureflowair.com/">Pureflowair https://pureflowair.com</a></li>
-                            <li><a target="_blank" href="https://www.mypoojabox.in/">Mypoojabox https://www.mypoojabox.in</a></li>
-                            <li><a target="_blank" href="https://www.u-lace.com/">Ulace https://www.u-lace.com</a></li>
-                            <li><a target="_blank" href="https://aashirvaadchakki.com/">Aashirvaadchakki https://aashirvaadchakki.com</a></li>
-                            <li><a target="_blank" href="https://www.haybeach.com/">Haybeach https://www.haybeach.com/</a></li>
-                            <li><a target="_blank" href="https://sowfoods.co/">SowFoods https://sowfoods.co/</a></li>
-                            <li><a target="_blank" href="https://strongandbrave.in/">Strong And Brave https://strongandbrave.in/</a></li>
-                            <li><a target="_blank" href="https://in.shop.lighting.philips.com/">Philips Lighting https://in.shop.lighting.philips.com/</a></li>
-                            <li><a target="_blank" href="https://byroe.com/">Byroe https://byroe.com/</a></li>
-                            <li><a target="_blank" href="https://sowfoods.co/">SowFoods https://sowfoods.co/</a></li>  
-                            <li><a target="_blank" href="https://www.merarang.com/">Merarang https://www.merarang.com/</a></li>  
-                            <li><a target="_blank" href="https://tensnet.com/">Tensnet https://tensnet.com/</a></li>  
+                            <li><a rel="nofollow" target="_blank" href="https://www.kawaiiamai.com">Kawaii Amai - https://www.kawaiiamai.com</a></li>
+                            <li><a rel="nofollow" target="_blank" href="https://mayandco.in">Mayandco - https://mayandco.in</a></li>
+                            <li><a rel="nofollow" target="_blank" href="https://www.newu.in">Newu - https://www.newu.in</a></li>
+                            <li><a rel="nofollow" target="_blank" href="https://jaqulineusa.com">Jaquline USA - https://jaqulineusa.com</a></li>
+                            <li><a rel="nofollow" target="_blank" href="https://amala.earth">Amala Earth - https://amala.earth</a></li>
+                            <li><a rel="nofollow" target="_blank" href="https://www.govo.life">Govo life - https://www.govo.life</a></li>
+                            <li><a rel="nofollow" target="_blank" href="https://www.biotique.com">Biotique - https://www.biotique.com</a></li>
+                            <li><a rel="nofollow" target="_blank" href="https://activbeaute.com">Active Beaute - https://activbeaute.com</a></li>
+                            <li><a rel="nofollow" target="_blank" href="https://www.yahvi.co.in">Yahvi - https://www.yahvi.co.in</a></li>
+                            <li><a rel="nofollow" target="_blank" href="https://chicasuu.com">Chicasuu - https://chicasuu.com</a></li>
+                            <li><a rel="nofollow" target="_blank" href="https://millesupergrain.com">MilleSuperGrain - https://millesupergrain.com</a></li>
+                            <li><a rel="nofollow" target="_blank" href="https://www.instax.in">Instax FujiFilms - https://www.instax.in</a></li>
+                            <li><a rel="nofollow" target="_blank" href="https://www.klfnirmal.com">KLF Nirmal : https://www.klfnirmal.com</a></li>
+                            <li><a rel="nofollow" target="_blank" href="https://membersonly.com">Members Only : https://membersonly.com</a></li>
+                            <li><a rel="nofollow" target="_blank" href="https://neutraleafindia.com">Neutraleaf India : https://neutraleafindia.com</a></li>
+                            <li><a rel="nofollow" target="_blank" href="https://ary-fj.myshopify.com">Araya : https://ary-fj.myshopify.com</a></li>
+                            <li><a rel="nofollow" target="_blank" href="#">Devatri Wellness</a></li>
+                            <li><a rel="nofollow" target="_blank" href="https://www.haldiramuk.com/">HaldiramsUk : https://www.haldiramuk.com/</a></li>
+                            <li><a rel="nofollow" target="_blank" href="https://main-hardwares.myshopify.com">Main Hardware : https://main-hardwares.myshopify.com</a></li>
+                            <li><a rel="nofollow" target="_blank" href="https://grabtocart-store.myshopify.com">Grab To Cart : https://grabtocart-store.myshopify.com</a></li>
+                            <li><a rel="nofollow" target="_blank" href="https://www.reranger.com">ReRanger : https://www.reranger.com</a></li>
+                            <li><a rel="nofollow" target="_blank" href="https://www.petland.ca/">Petland Canada : https:// petland.ca</a></li>
+                            <li><a rel="nofollow" target="_blank" href="https://randolphusa.com">Randolph USA : https://randolphusa.com</a></li>
+                            <li><a rel="nofollow" target="_blank" href="https://yufoodlabs.com">yufoodlabs : https://yufoodlabs.com</a></li>
+                            <li><a rel="nofollow" target="_blank" href="https://xtepindia.com/">Xtep : https://xtepindia.com/</a></li>
+                            <li><a rel="nofollow" target="_blank" href="https://ebec94-3.myshopify.com/">Boito https://ebec94-3.myshopify.com</a></li>
+                            <li><a rel="nofollow" target="_blank" href="https://confimen.in/">Confimen https://confimen.in</a></li>
+                            <li><a rel="nofollow" target="_blank" href="https://pureflowair.com/">Pureflowair https://pureflowair.com</a></li>
+                            <li><a rel="nofollow" target="_blank" href="https://www.mypoojabox.in/">Mypoojabox https://www.mypoojabox.in</a></li>
+                            <li><a rel="nofollow" target="_blank" href="https://www.u-lace.com/">Ulace https://www.u-lace.com</a></li>
+                            <li><a rel="nofollow" target="_blank" href="https://aashirvaadchakki.com/">Aashirvaadchakki https://aashirvaadchakki.com</a></li>
+                            <li><a rel="nofollow" target="_blank" href="https://www.haybeach.com/">Haybeach https://www.haybeach.com/</a></li>
+                            <li><a rel="nofollow" target="_blank" href="https://sowfoods.co/">SowFoods https://sowfoods.co/</a></li>
+                            <li><a rel="nofollow" target="_blank" href="https://strongandbrave.in/">Strong And Brave https://strongandbrave.in/</a></li>
+                            <li><a rel="nofollow" target="_blank" href="https://in.shop.lighting.philips.com/">Philips Lighting https://in.shop.lighting.philips.com/</a></li>
+                            <li><a rel="nofollow" target="_blank" href="https://byroe.com/">Byroe https://byroe.com/</a></li>
+                            <li><a rel="nofollow" target="_blank" href="https://sowfoods.co/">SowFoods https://sowfoods.co/</a></li>  
+                            <li><a rel="nofollow" target="_blank" href="https://www.merarang.com/">Merarang https://www.merarang.com/</a></li>  
+                            <li><a rel="nofollow" target="_blank" href="https://tensnet.com/">Tensnet https://tensnet.com/</a></li>  
+                            <li><a rel="nofollow" target="_blank" href="https://lifelongindiaonline.com/">LifeLong https://lifelongindiaonline.com/</a></li>  
+                            <li><a rel="nofollow" target="_blank" href="https://viva-vetro.com/">Viva Vetro https://viva-vetro.com/</a></li>  
+                            <li><a rel="nofollow" target="_blank" href="https://gaymedlabs.com/">GLPL https://gaymedlabs.com/</a></li>  
+                            <li><a rel="nofollow" target="_blank" href="https://www.theeventtales.in/">TheEventTales https://www.theeventtales.in/</a></li>  
                         </ul>
-                    </div>
+                    </div>  
                 </section>
             </main>
         </Layout>
